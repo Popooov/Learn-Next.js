7c59986fd579928db51f281aeef5c208b6cdcbef
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-03-24 22:08:24 +0000

    feat: Add experimental serverActions with allowedOrigins configuration

--- nextjs-dashboard/next.config.js
+++ nextjs-dashboard/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["gc8tw3-3000.csb.app"],
+        }
+    },
+};
 
 module.exports = nextConfig;
